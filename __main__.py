43a9ab9e2c318a5e79dec81030cb87e862a1f76e
3ricsonn <3ricsonn@protonmail.com>
2025-08-01 13:32:00 +0200

    add ability to generate basic data packs

--- __main__.py
+++ __main__.py
@@ -14,17 +14,69 @@ class SmartFormatter(argparse.HelpFormatter):
         return argparse.HelpFormatter._split_lines(self, text, width)
 
 def creat_datapack(args: argparse.Namespace):
-    print("datapack")
+    # create datapack folder
+    datapack_folder = os.path.join(cwd, args.name[0])
+
+    # create /data/minecraft/tags/functions folder
+    entry_points_folder = os.path.join(datapack_folder, "data", "minecraft", "tags", "functions")
+
+    # create /namespace/functions folder
+    namespace = args.namespace[0] if args.namespace else args.name[0].lower()
+    namespace_folder = os.path.join(datapack_folder, "data", namespace, "functions")
+
+    if args.dry_run:
+        print(datapack_folder)
+
+        # create meta-data file
+        print(os.path.join(datapack_folder, "pack.mcmeta"))
+        print(f'pack.mcmeta: {{"pack":{{"pack_format":{args.version[0]},"description":"{args.name[0]} datapack"}}}}')
+
+        print(entry_points_folder)
+        print(namespace_folder)
+
+        # create templates
+        if args.template:
+            print(os.path.join(entry_points_folder, "load.json"))
+            print(f'load.json: {{"values": ["{namespace}:load"]}}')
+            print(os.path.join(entry_points_folder, "tick.json"))
+            print(f'tick.json: {{"values": ["{namespace}:tick"]}}')
+
+            print(os.path.join(namespace_folder, "load.mcfunction"))
+            print("load.mcfunction: msg @a Hello World!")
+            print(os.path.join(namespace_folder, "tick.mcfunction"))
+            print("tick.mcfunction: # tick function")
+    else:
+        os.mkdir(datapack_folder)
+
+        # create meta-data file
+        with open(os.path.join(datapack_folder, "pack.mcmeta"), "a") as file:
+            file.write(
+                f'{{"pack":{{"pack_format":{args.version[0]},"description":"{args.name[0]} datapack"}}}}'
+            )
+
+        os.makedirs(entry_points_folder)
+        os.makedirs(namespace_folder)
+
+        # create templates
+        if args.template:
+            with open(os.path.join(entry_points_folder, "load.json"), "a") as file:
+                file.write(f'{{"values": ["{namespace}:load"]}}')
+            with open(os.path.join(entry_points_folder, "tick.json"), "a") as file:
+                file.write(f'{{"values": ["{namespace}:tick"]}}')
+
+            with open(os.path.join(namespace_folder, "load.mcfunction"), "a") as file:
+                file.write("msg @a Hello World!")
+            with open(os.path.join(namespace_folder, "tick.mcfunction"), "a") as file:
+                file.write("# tick function")
 
 
 def create_resourcepack(args: argparse.Namespace):
     print("resourcepack")
 
 if __name__ =="__main__":
-    #ToDo: find a suitable name for the program
     parser = argparse.ArgumentParser(
-        prog="test",
-        description="NAME creates a basic directory structure for a new mc-datapack "
+        prog="mc-pack-template",
+        description="mc-pack-template creates a basic directory structure for a new mc-datapack "
              "or mc-resourcepack project",
      formatter_class=SmartFormatter
     )
